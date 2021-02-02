9ee1de0e95ae321518a41a02b6b39b3333c018aa
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-02 15:39:09 -0600

    nodemon package installed and configured

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 app.get("/add", function(req, res) {
