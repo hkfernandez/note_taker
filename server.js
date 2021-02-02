0730a21dd9945bd7746b47cdb3eb99abf4f8466e
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-02 15:47:12 -0600

    server.js - clicking Start Button on index.html takes user to notes.html

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 // Displays all characters
