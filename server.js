4ace08dbee3ba819f0e0408637e00c498c7636f0
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-03 09:36:57 -0600

    server.js - clean up console logs

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var path = require("path");
 var fs = require('fs');
-// var fs = require ('fs');
 
 var app = express();
 var PORT = 8080;
@@ -15,18 +14,13 @@ app.use(express.static('public'));
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// send user to home page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-
+// send user to notes page
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
-});
-
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
 });
 
 // returns list of notes
@@ -51,19 +45,22 @@ app.post("/api/notes", function(req, res) {
       res.json(newNote);
    });
 app.delete("/api/notes/:id", function(req, res) {
-      console.log('working');
-      console.log(req.params);
-      // newNote.id = new Date();
-      // console.log('note with id ' +newNote);
-      // let data = fs.readFileSync(__dirname+"/db/db.json")
-      // console.log(data);
-      // let noteList = JSON.parse(data);
-      // console.log(noteList);
-      // noteList.push(newNote);
-      // fs.writeFile(__dirname+"/db/db.json", JSON.stringify(noteList), (noteData)=>{
-      //       console.log('showing data'+noteData);
-      // })
-      // res.json(newNote);
+      let idToDelete = req.params
+      let data = fs.readFileSync(__dirname+"/db/db.json")
+      let noteList = JSON.parse(data);
+      console.log('old note list: '+noteList);
+      console.log(idToDelete);
+      for (let i = 0; i < noteList.length; i++) {
+            console.log(noteList[i].id);
+            if (noteList[i].id === idToDelete.id){
+                  noteList.splice([i],1);
+                  console.log('new note list: '+noteList);
+            }
+      }
+      fs.writeFile(__dirname+"/db/db.json", JSON.stringify(noteList), (noteData)=>{
+            // console.log('showing data'+noteData);
+      })
+      res.json(idToDelete);
    });
 
 // Starts the server to begin listening
